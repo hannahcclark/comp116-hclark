259aef8aaee59c0aba71c3e7f151ddcee1cca4ba
hannahcclark <hannah_clark@mac.com>
2015-09-24 17:26:03 -0400

    got nikto, cards

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -12,6 +12,7 @@ class Alarm
                                        :promisc => true)
         stream.stream.each do |raw|
             packet = PacketFu::Packet.parse(raw)
+            puts "***************************************"
             if packet.is_a?(PacketFu::TCPPacket)
                 flags = packet.tcp_flags.to_i()
                 if flags == 0
@@ -20,6 +21,14 @@ class Alarm
                     puts "FIN"
                 elsif flags == 41
                     puts "XMAS"
+                end
+            end
+            if packet.payload() =~ /(.*)Nikto(.*)/
+                puts "Nikto"
+            end
+            if (packet.payload() =~ 
+                 /(.*)(((3|4|5)\d{3})|6011)(((-| )?\d{4}){3})(.*)/)
+                puts "Credit Card
             end
         end
     end
