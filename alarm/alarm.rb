0921ad9d23c0c5876742f463ff3cc8c69628b531
hannahcclark <hannah_clark@mac.com>
2015-10-10 21:09:02 -0400

    added server incident detection and new nmap scan options

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -3,10 +3,32 @@ require 'packetfu'
 class Alarm
 
     def search_log(logfile)
-
+        incidents = 0
+        File.readlines(logfile).each do |line|
+            if line =~ /Nikto/
+                incidents += 1
+                puts "Nikto"
+            elsif line =~ /((php)+(myadmin))|(myadmin)(php)+/i
+                incidents += 1
+                puts "phpmyadmin"
+            elsif line =~ /masscan/
+                incidents += 1
+                puts "masscan"
+            elsif line =~ /nmap/i
+                incidents += 1
+                puts "nmap"
+            elsif line =~ /(shellshock-scan)|(\(\)\s*\{\s*:;\s*\}\; ping -c)/
+                incidents += 1
+                puts "shellshock"
+            elsif line =~ /\/bin\/(bash|sh|perl)/
+                incidents += 1
+                puts "shellcode"
+            end
+        end
     end
 
     def sniff_packets(interface)
+        incidents = 0
         stream = PacketFu::Capture.new(:start => true,
                                        :iface => interface,
                                        :promisc => true)
@@ -16,27 +38,43 @@ class Alarm
             if packet.is_a?(PacketFu::TCPPacket)
                 flags = packet.tcp_flags.to_i()
                 if flags == 0
+                    incidents += 1
                     puts "NULL"
                 elsif flags == 1
+                    incidents += 1
                     puts "FIN"
+                elsif flags == 3
+                    incidents += 1
+                    puts "Maimon nmap scan"
+                elsif flags == 
                 elsif flags == 41
-                    puts "XMAS"
+                    incidents += 1
+                    puts "Xmas"
                 end
             end
-            if packet.payload() =~ /(.*)Nikto(.*)/
+            if packet.payload() =~ /Nikto/
+                incidents += 1
                 puts "Nikto"
-            end
-            if packet.payload() =~ \
-                 /(.*)(((3|4|5)\d{3})|6011)(((-| )?\d{4}){3})(.*)/
+            elsif packet.payload( =~ /nmap/
+                incidents += 1
+                puts "nmap"
+            elsif packet.payload() =~ \
+                 /(((3|4|5)\d{3})|6011)(((-| )?\d{4}){3})/
+                incidents += 1
                 puts "Credit Card"
             end
-            puts packet.payload()
         end
     end
 end
 
 if __FILE__ == $0
     alarm = Alarm.new()
-    alarm.sniff_packets('eth0')
+    if ARGV.length == 2 and ARGV[0] == "-r"
+        alarm.search_log(ARGV[1])
+    elsif ARGV.length == 0
+        alarm.sniff_packets('eth0')
+    else
+        puts "Invalid Arguments"    
+    end
 end
 
